106193a0b5627c2664cd82784dd8d990b0d332a9
Nigel <nigelsilva12@gmail.com>
2018-12-20 22:24:37 -0500

    Update settings.py

--- hmwyp/settings.py
+++ hmwyp/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'hmwyp/static'),
+    os.path.join(BASE_DIR, 'hmwypapp/static'),
 )
 
